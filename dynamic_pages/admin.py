9222e7b7fb216e18f5f32f19080ebf3698d1824f
= <matllubos@gmail.com>
2012-08-17 18:06:41 +0200

    Changed class names in django-simple-utilities

--- dynamic_pages/admin.py
+++ dynamic_pages/admin.py
@@ -3,12 +3,12 @@ from django.contrib import admin
 from django.contrib.admin.sites import AlreadyRegistered
 from django.db.models import get_models
 
-from utilities.admin import TreeModelAdmin, HiddenModelAdmin
+from utilities.admin import TreeModelMixin, RelatedToolsAdmin, HiddenModelAdmin
 
 from form.page import PageForm
 from models import Page, Meta, PageContent
     
-class PageAdmin(TreeModelAdmin):
+class PageAdmin(TreeModelMixin, RelatedToolsAdmin):
     list_display = ('title','absolute_url','url', 'page_type', 'order')
     form = PageForm
     parent = 'parent'
