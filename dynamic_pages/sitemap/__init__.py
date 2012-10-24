41c0ce297bf56188d40a109f7a74dc5a5f392674
= <matllubos@gmail.com>
2012-10-24 16:37:20 +0200

    Site map first commit

---
+++ dynamic_pages/sitemap/__init__.py
@@ -0,0 +1,81 @@
+# coding: utf-8
+import re
+
+from django.contrib.sitemaps import Sitemap
+
+from django.utils.encoding import smart_unicode, force_unicode
+from django.core import urlresolvers, paginator
+
+from dynamic_pages.models import Page
+
+
+class Siteurls(object):
+    
+    def get_urls(self, pattern):
+        return []
+
+
+    def get_sitemaps(self, pattern, updated):
+        urls = self.get_urls(pattern)
+        for url in urls:
+            if not re.search('[\[\?\+\*\.\]]', url):
+                url = re.sub('[\(\)]', '', url)
+                yield SitemapUrl(updated, url)
+        
+
+class ModelFieldSiteurls(Siteurls):
+    def __init__(self, queryset, field='pk'):
+        self.queryset = queryset
+        self.field = field
+        
+    def get_urls(self, pattern):
+        for obj in self.queryset:
+            yield re.sub('(\([^)]*\))', smart_unicode(getattr(obj, self.field)), pattern, count=1)
+
+    
+class SitemapUrl(object):
+    def __init__(self, updated, url):
+        self.updated = updated
+        self.url = url
+        
+
+class StaticPagesSitemap(Sitemap):
+
+    def __init__(self, data={}):
+        self.data = data
+        
+    def items(self):
+        items = []
+        
+        from dynamic_pages.dynamic.utils import get_dynamic_url_by_choice
+        
+        for page in Page.objects.all().order_by('parent', 'order'):
+            dynamic_url = get_dynamic_url_by_choice(page.page_type)
+            i = 0
+            for pattern in dynamic_url.get_patterns(page):     
+                pattern = re.sub('^\^', '', pattern) 
+                pattern = re.sub('\$$', '', pattern) 
+                pattern = re.sub('/\?', '', pattern) 
+
+                if not re.search('[\[\?\+\*\.\]]', pattern):
+                    pattern = re.sub('[\(\)]', '', pattern)
+                    items.append(SitemapUrl(page.updated, pattern))
+                elif self.data.has_key(page.page_type_name) and len(self.data[page.page_type_name]) > i:
+                    items.extend(self.data[page.page_type_name][i].get_sitemaps(pattern, page.updated))
+                    i+=1
+                    
+            if len(dynamic_url.get_patterns(page)) == 0 and not page.page_type_name in ['redirects', 'redirectstourl', 'redirectstopage',]:
+                items.append(SitemapUrl(page.updated, page.absolute_url))
+
+        return items
+
+    
+    def _get_paginator(self):
+        return paginator.Paginator(self.items(), self.limit)
+    paginator = property(_get_paginator)
+    
+    def lastmod(self, obj):
+        return obj.updated
+    
+    def location(self, obj):
+        return '/%s' % obj.url
