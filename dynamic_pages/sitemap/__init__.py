c456866b4663a56502b4c85fe1cafb89e1ed2632
= <matllubos@gmail.com>
2012-10-29 16:59:56 +0100

    Removed prints

--- dynamic_pages/sitemap/__init__.py
+++ dynamic_pages/sitemap/__init__.py
@@ -40,56 +40,14 @@ class SitemapUrl(object):
         self.updated = updated
         self.url = url
         
-
-class StaticPagesSitemap(Sitemap):
-
-    def __init__(self, data={}):
-        self.data = data
-        
-    def items(self):
-        items = []
-       
-        from dynamic_pages.dynamic.utils import get_dynamic_url_by_choice
-        
-        for page in Page.objects.all().order_by('parent', 'order'):
-            dynamic_url = get_dynamic_url_by_choice(page.page_type)
-            i = 0
-            for pattern in dynamic_url.get_patterns(page):     
-                pattern = re.sub('^\^', '', pattern) 
-                pattern = re.sub('\$$', '', pattern) 
-                pattern = re.sub('/\?', '', pattern) 
-
-                if not re.search('[\[\?\+\*\.\]]', pattern):
-                    pattern = re.sub('[\(\)]', '', pattern)
-                    items.append(SitemapUrl(page.updated, pattern))
-                elif self.data.has_key(page.page_type_name) and len(self.data[page.page_type_name]) > i:
-                    items.extend(self.data[page.page_type_name][i].get_sitemaps(pattern, page.updated))
-                    i+=1
-                    
-        return items
-
-    
-    def _get_paginator(self):
-        return paginator.Paginator(self.items(), self.limit)
-    paginator = property(_get_paginator)
-    
-    def lastmod(self, obj):
-        return obj.updated
-    
-    def location(self, obj):
-        return '/%s' % obj.url
-    
     
 class DefaultPageSitemap(Sitemap):
 
     def __init__(self, page, data = []):
         self.page = page
         self.data = data
-        print data
         
     def items(self):
-        print 'ted'
-        print self.data
         items = []
        
         from dynamic_pages.dynamic.utils import get_dynamic_url_by_choice
